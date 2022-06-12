504dbb15f9ab551d03fc3da4bed9f68976029a20
Arun <89001976+S-V-ARUN@users.noreply.github.com>
2022-06-12 10:41:30 +0530

    Add files via upload

---
+++ Hands On/11.06.2022 (Delegate)/Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public delegate void TablesDelegate();
+namespace DelegateTest
+{
+    class TablesClass
+    {
+        public static void Tables3()
+        {
+            Console.WriteLine("3 tables to 10");
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("3*{0}={1}",i,3*i);
+            }
+        }
+        public static void Tables5()
+        {
+            Console.WriteLine("5 tables to 10");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("5*{0}={1}", i, 5 * i);
+            }
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            TablesDelegate t1 = new TablesDelegate(TablesClass.Tables3);
+            t1 += TablesClass.Tables5;
+            t1.Invoke();
+        }
+    }
+
+}

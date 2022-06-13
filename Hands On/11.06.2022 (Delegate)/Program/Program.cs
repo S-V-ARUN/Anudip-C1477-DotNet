c1fdb7dec4f37db52e35004cbdbd4cf81607b2c6
Arun <89001976+S-V-ARUN@users.noreply.github.com>
2022-06-13 23:07:16 +0530

    Update Program.cs

--- Hands On/11.06.2022 (Delegate)/Program/Program.cs
+++ Hands On/11.06.2022 (Delegate)/Program/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+//Running two methods using delegate
+//declaring Delegate
 public delegate void TablesDelegate();
 namespace DelegateTest
 {
     class TablesClass
     {
+        //method for two table calculation
         public static void Tables3()
         {
             Console.WriteLine("3 tables to 10");
@@ -12,6 +15,7 @@ namespace DelegateTest
                 Console.WriteLine("3*{0}={1}",i,3*i);
             }
         }
+        //method for three table calculation
         public static void Tables5()
         {
             Console.WriteLine("5 tables to 10");
@@ -25,8 +29,11 @@ namespace DelegateTest
     {
         static void Main(string[] args)
         {
+            //creating object for delegate
             TablesDelegate t1 = new TablesDelegate(TablesClass.Tables3);
+            //binding method to delegate
             t1 += TablesClass.Tables5;
+            //invoking delegate
             t1.Invoke();
         }
     }

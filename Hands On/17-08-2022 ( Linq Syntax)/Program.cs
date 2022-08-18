b4776eff964fd11a70eb393df68061edbc10cb38
Arun <89001976+S-V-ARUN@users.noreply.github.com>
2022-08-18 20:53:17 +0530

    Add files via upload

---
+++ Hands On/17-08-2022 ( Linq Syntax)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+namespace Create
+{
+    class LINq
+    {
+        public static void Main(String[] args)
+        {
+            ArrayList a = new ArrayList();
+            a.Add(100);
+            a.Add("arun");
+            a.Add(2.0);
+            a.Add("DotNet");
+            ArrayList a1 = new ArrayList();
+            a1.Add(100);
+            a1.Add(2000);
+            a1.Add(50);
+            a1.Add(10);
+            //Linq Syntax
+            var b = from c in a.OfType<String>() select c;
+            var d = from int f in a1 where f>50 select f;
+            foreach (var c in b)
+            {
+                Console.WriteLine(c);
+            }
+            foreach (int c in d)
+            {
+                Console.WriteLine(c);
+            }
+               
+                
+        }
+
+    }
+
+
+}
